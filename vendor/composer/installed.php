7143e40d92e5b4f46712cdaf9ed8423fa7f73d45
liz972 <Wang889900001@yahoo.com>
2025-05-27 08:04:46 +0000

    Force Render to treat as PHP with setup.sh

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b7d86d0273d593819d5982c29a11f4d2e620a6fb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b7d86d0273d593819d5982c29a11f4d2e620a6fb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
